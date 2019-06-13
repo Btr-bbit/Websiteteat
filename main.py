df9b9956b658368eb37f64e5339a5d548fb6eaf1
Btr-bbit <39153134+Btr-bbit@users.noreply.github.com>
2019-06-13 22:10:57 +0800

    commit test

---
+++ main.py
@@ -0,0 +1,11 @@
+rom flask import Flask
+
+app = Flask(__name__,static_url_path='index.html')    
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8081)
